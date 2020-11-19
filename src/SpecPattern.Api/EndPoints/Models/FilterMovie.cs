05fcf2df00ed76e9471a0cd700444b90a5d516b3
Francisco Jose Fernandez Garcia <franjfgcarmo@gmail.com>
2020-11-19 10:22:17 +0100

    implemented new features.

---
+++ src/SpecPattern.Api/EndPoints/Models/FilterMovie.cs
@@ -0,0 +1,9 @@
+﻿namespace SpecPattern.Api.EndPoints.Models
+{
+    public class FilterMovie
+    {
+        public bool ForKidsOnly { get; set; }
+        public double MinimunRating { get; set; }
+        public bool OnCd { get; set; }
+    }
+}
--- src/SpecPattern.Api/EndPoints/MovieController.cs
+++ src/SpecPattern.Api/EndPoints/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SpecPattern.Api.EndPoints.Models;
 using SpecPattern.Domain;
 using SpecPattern.Domain.Entities;
 
@@ -22,8 +23,14 @@ namespace SpecPattern.Api.EndPoints
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAsync() {
-            var result = await _unitOfWork.Repository<Movie>().AllAsync();
+        public async Task<IActionResult> GetAsync(FilterMovie filterMovie)
+        {
+            var result = await _unitOfWork.Repository<Movie>()
+                .FindAsync(w =>
+                (w.MpaaRating <= MpaaRating.PG || !filterMovie.ForKidsOnly) &&
+                w.Rating >= filterMovie.MinimunRating &&
+                (w.ReleaseDate <= DateTime.Now.AddMonths(-12) || !filterMovie.OnCd)
+                );
             return result is null ? NotFound() : (IActionResult)Ok(result.ToList());
         }
 
@@ -33,6 +40,39 @@ namespace SpecPattern.Api.EndPoints
         {
             var result = await _unitOfWork.Repository<Movie>().GetAsync(id);
             return result is null ? NotFound() : (IActionResult)Ok(result);
+        }
+
+        [HttpPost]
+        [Route("buy-adult-ticket/id:{int}")]
+        public async Task<IActionResult> BuyAdultTicket(int movieId)
+        {
+            var result = await _unitOfWork.Repository<Movie>().GetAsync(movieId);
+            return result is null ? NotFound() : (IActionResult)Ok(result);
+        }
+        [HttpPost]
+        [Route("buy-child-ticket/id:{int}")]
+        public async Task<IActionResult> BuyChildTicket(int movieId)
+        {
+            var result = await _unitOfWork.Repository<Movie>().GetAsync(movieId);
+            if (result is null)
+                return NotFound();
+            if (result.MpaaRating > MpaaRating.PG) {
+                return Conflict("The mvie is not sutable for children");
+            }
+            return Ok(result);
+        }
+        [HttpPost]
+        [Route("buy-cd/id:{int}")]
+        public async Task<IActionResult> BuyCd(int movieId)
+        {
+            var result = await _unitOfWork.Repository<Movie>().GetAsync(movieId);
+            if (result is null)
+                return NotFound();
+            if (result.ReleaseDate <= DateTime.Now.AddMonths(-1))
+            {
+                return Conflict("The movie doesn´t have a CD version");
+            }
+            return Ok(result);
         }
     }
 }
